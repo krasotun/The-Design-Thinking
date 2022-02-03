1b51e527c6970651810dcd35549407214f2d13c0
Marat <servrostov@gmail.com>
2022-02-03 17:50:53 +0300

    add Реализовал табы в секции Marketing

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log('Привет, Марат!');
+// TABS
+// Выбираем табы в секции Marketing
+// Используем event-delegation
+// Чтобы не вешать на каждый таб свой EvenTlistner
+// Мы вешаем  их на контейнер
+
+// Выбираем контейнер с табами
+const marketingTabContainer = document.querySelector('.tab__container_place_marketing');
+
+// Выбираем ВСЕ табы в контейнере (придет Node список)
+const marketingTags = document.querySelectorAll('.tab_place_marketing');
+
+// Пишем функцию для Парсинга клика именно по классу
+const parseTagFromContainer = function () {
+  marketingTabContainer.addEventListener('click', (e) => { // (e) это Event клик
+    if (e.target.classList.contains("tab")) { // Мы обрабатываем событие клик и берем из него параметры (target)
+      let clickedTag = e.target;
+      toggleMarketingTag(clickedTag); // Вызываем функцию для toggle класса
+    }
+  });
+};
+
+parseTagFromContainer();
+
+// Пишем функцию по замене класса у кликнутого тэга
+const toggleMarketingTag = function (a) {
+  a.classList.toggle('tab_selected');
+};
