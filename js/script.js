97aaa4ad8e6716aef12a5308ae6d8b8979d09ce8
krasotun <servrostov@gmail.com>
2022-02-11 00:05:50 +0300

    add Реализовал табы

--- js/script.js
+++ js/script.js
@@ -15,17 +15,18 @@ const marketingArticlesContainers = document.querySelectorAll('.strategy__contai
 
 // Функция скрывает ВСЕ статьи (добавляя класс к контейнеру)
 const hideArticles = function () {
-  for (let i = 0; i < (marketingArticlesContainers.length); i++) {
-    marketingArticlesContainers[i].classList.add('strategy__container_hidden');
-  }
+  marketingArticlesContainers.forEach(article => {
+    article.classList.add('strategy__container_hidden');
+  });
 };
 
 // Функция показывает ВСЕ  статьи
 const showArticles = function () {
-  for (let i = 0; i < (marketingArticlesContainers.length); i++) {
-    marketingArticlesContainers[i].classList.remove('strategy__container_hidden');
-  }
+  marketingArticlesContainers.forEach(article => {
+    article.classList.remove('strategy__container_hidden');
+  });
 };
+
 
 // Функция убирает selected у всех табов, у кого он есть
 const removeMarketingSelectedClass = function () {
@@ -33,38 +34,37 @@ const removeMarketingSelectedClass = function () {
     if (marketingTabs[i].classList.contains('tab_selected')) {
       marketingTabs[i].classList.remove('tab_selected');
     }
-
   }
 };
 
-
-// Пишем функцию для обработки клика именно по тегу (Event delegation)
-const clickTagFromContainer = function () {
+// Пишем функцию для переключения статей по выбранному тегу
+const selectArticlesByTag = function () {
   marketingTabContainer.addEventListener('click', (e) => { // (e) это Event клик
     if (e.target.classList.contains("tab")) { // Мы обрабатываем событие клик и берем из него параметры (target)
       let clickedTag = e.target;
       removeMarketingSelectedClass(); // Убираем selected
       toggleMarketingTag(clickedTag); // Вызываем функцию для toggle класса (выделение цвета)
       hideArticles(); // Скрываем все статьи
-      filterArticleByTag();
+      marketingArticlesContainers.forEach(article => { // Перебираем все статьи
+        article.querySelectorAll('.tab_place_strategy').forEach(tag => { // Перебираем все теги в каждой статье
+          if (tag.innerText === e.target.innerText) { // Сравниваем выбранный тег с тегом статьи
+            article.classList.remove('strategy__container_hidden'); // если совпадает то показываем статью
+          } else if (e.target.innerText === 'All') { // Если клик по табу All
+            showArticles(); // показываем все статьи
+          }
+        });
+      });
     }
   });
 };
-clickTagFromContainer(); // Вызываем функцию
-
-// Пишем функцию для выбора статей по тегу
-const filterArticleByTag = function () {
-  marketingArticlesContainers.forEach(strategy => {
-
-  });
-};
-
-
+selectArticlesByTag(); // Вызываем функцию
 
 // Пишем функцию по замене класса у кликнутого тэга
 const toggleMarketingTag = function (a) {
   a.classList.toggle('tab_selected');
 };
+
+
 
 // MODALS
 // Выбираем контейнер со статьями
